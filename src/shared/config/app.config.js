73c371da2f1538078e711ef1c0646207f37f130c
Edicson Armando Hurtado Lozano <edhurtad@Nequi.com>
2026-05-14 08:22:30 -0500

    refactor:Ajuste de url webhook

--- src/shared/config/app.config.js
+++ src/shared/config/app.config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  webhookUrl: 'https://delfosn8n-webhook-datalab.nequi.io/webhook/soat-simulador',
+  webhookUrl: 'https://delfosn8n-datalab.nequi.io/webhook-test/soat-simulador',
   mosparo: {
     host: 'https://captcha-pdn.bancadigital.com.co',
     uuid: '6ed07a5e-e5a5-481e-8b3e-10207103e394',
